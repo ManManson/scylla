1106ffba0a01c00d3e23d115a2e69be05b5710a1
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2020-11-25 11:58:40 +0300

    raft: copy comments from raft.hh to idl/raft.idl.hh
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- idl/raft.idl.hh
+++ idl/raft.idl.hh
@@ -33,7 +33,7 @@ struct tagged_uint64 {
     uint64_t get_value();
 };
 
-}
+} // namespace internal
 
 struct server_address {
     raft::server_id id;
@@ -41,30 +41,44 @@ struct server_address {
 };
 
 struct configuration {
+    // Used during the transitioning period of configuration
+    // changes.
     std::unordered_set<raft::server_address> previous;
+    // Contains the current configuration. When configuration
+    // change is in progress, contains the new configuration.
     std::unordered_set<raft::server_address> current;
 };
 
 struct snapshot {
+    // Index and term of last entry in the snapshot
     raft::index_t idx = raft::index_t(0);
     raft::term_t term = raft::term_t(0);
+    // The committed configuration in the snapshot
     raft::configuration config;
+    // Id of the snapshot.
     raft::snapshot_id id;
 };
 
 struct vote_request {
+    // The candidate’s term.
     raft::term_t current_term;
+    // The index of the candidate's last log entry.
     raft::index_t last_log_idx;
+    // The term of the candidate's last log entry.
     raft::term_t last_log_term;
 };
 
 struct vote_reply {
+    // Current term, for the candidate to update itself.
     raft::term_t current_term;
+    // True means the candidate received a vote.
     bool vote_granted;
 };
 
 struct install_snapshot {
+    // Current term on a leader
     raft::term_t current_term;
+    // A snapshot to install
     raft::snapshot snp;
 };
 
@@ -74,17 +88,31 @@ struct snapshot_reply {
 
 struct append_reply {
     struct rejected {
+        // Index of non matching entry that caused the request
+        // to be rejected.
         raft::index_t non_matching_idx;
+        // Last index in the follower's log, can be used to find next
+        // matching index more efficiently.
         raft::index_t last_idx;
     };
     struct accepted {
+        // Last entry that was appended (may be smaller than max log index
+        // in case follower's log is longer and appended entries match).
         raft::index_t last_new_idx;
     };
+    // Contains an index of the last commited entry on the follower
+    // It is used by a leader to know if a follower is behind and issuing
+    // empty append entry with updates commit_idx if it is
+    // Regular RAFT handles this by always sending enoty append requests 
+    // as a hearbeat.
     raft::index_t commit_idx;
     std::variant<raft::append_reply::rejected, raft::append_reply::accepted> result;
 };
 
 struct log_entry {
+    // Dummy entry is used when a leader needs to commit an entry
+    // (after leadership change for instance) but there is nothing
+    // else to commit.
     struct dummy {};
 
     raft::term_t term;
