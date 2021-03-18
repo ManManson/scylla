5a3191a3d563c7cadbea142e305125c6ab1870da
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2021-03-18 12:08:22 +0300

    raft: wire up `rpc::add_server` and `rpc::remove_server` for configuration changes
    Raft instance needs to update RPC subsystem on changes in
    configuration, so that RPC can deliver messages to the new nodes
    in configuration, as well as dispose of the old nodes.
    I.e. the nodes which are not the part of the most recent
    configuration anymore.
    
    The effective scope of RPC mappings is limited by the piece of
    code which sends messages to both the "new" nodes (which
    are added to the cluster with the most recent configuration
    change) and the "old" nodes which are removed from the cluster.
    
    Until the messages are successfully delivered to at least
    the majority of "old" nodes and we have heard back from them,
    the mappings should be kept intact. After that point the RPC
    mappings for the removed nodes are no longer of interest
    and thus can be immediately disposed.
    
    Tests: unit(dev)
    
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- raft/fsm.cc
+++ raft/fsm.cc
@@ -283,7 +283,16 @@ fsm_output fsm::get_output() {
     // to not lose messages in case arrays population throws
     std::swap(output.messages, _messages);
 
-    if (_observed._last_conf_idx != _log.last_conf_idx()) {
+    // Fill server_address_set corresponding to the configuration from
+    // the rpc point of view.
+    //
+    // Effective rpc configuration changes when one of the following applies:
+    // * `last_conf_idx()` could have changed or
+    // * A new configuration entry may be overwritten by application of two
+    //   snapshots with different configurations following each other.
+    // * Leader overwrites a follower's log.
+    if (_observed._last_conf_idx != _log.last_conf_idx() ||
+            _observed._current_term != _log.last_term()) {
         configuration last_log_conf = _log.get_configuration();
         last_log_conf.current.merge(last_log_conf.previous);
         output.rpc_configuration = last_log_conf.current;
