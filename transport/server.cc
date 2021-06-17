17c3f57ba512a93ed2340c187e704ae98159d62e
Pavel Solodovnikov <pa.solodovnikov@scylladb.com>
2021-06-17 09:46:54 +0300

    fixup
    Signed-off-by: Pavel Solodovnikov <pa.solodovnikov@scylladb.com>

--- transport/server.cc
+++ transport/server.cc
@@ -857,7 +857,7 @@ cql_server::connection::process_on_shard(::shared_ptr<messages::result_message::
         service::client_state& cs, service_permit permit, tracing::trace_state_ptr trace_state, Process process_fn) {
     return _server.container().invoke_on(*bounce_msg->move_to_shard(), _server._config.bounce_request_smp_service_group,
             [this, is = std::move(is), cs = cs.move_to_other_shard(), stream, permit = std::move(permit), process_fn,
-             gt = tracing::global_trace_state_ptr(std::move(trace_state)), cached_vals = std::move(bounce_msg->cached_function_calls())] (cql_server& server) mutable {
+             gt = tracing::global_trace_state_ptr(std::move(trace_state)), cached_vals = std::move(std::move(*bounce_msg).cached_function_calls())] (cql_server& server) mutable {
         service::client_state client_state = cs.get();
         return do_with(bytes_ostream(), std::move(client_state), std::move(cached_vals), [this, &server, is = std::move(is), stream, process_fn,
                                                                   trace_state = tracing::trace_state_ptr(gt)]
